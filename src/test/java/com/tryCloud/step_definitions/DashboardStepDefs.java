9fa928b72b2183b7f782cbbf969e902dc13f982d
kana1983 <kana.stasiukiewicz@gmail.com>
2024-10-31 18:19:54 -0400

    user story changes

---
+++ src/test/java/com/tryCloud/step_definitions/DashboardStepDefs.java
@@ -0,0 +1,161 @@
+package com.tryCloud.step_definitions;
+
+import com.tryCloud.pages.DashboardPage;
+import com.tryCloud.utilities.BrowserUtils;
+import com.tryCloud.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class DashboardStepDefs {
+
+    DashboardPage dashboardPage = new DashboardPage();
+
+    @When("user can see all modules")
+    public void userCanSeeAllModules(List<String> listOfModules) {
+        System.out.println("listOfModules = " + listOfModules);
+
+    }
+
+    @When("user can see username on dashboard")
+    public void userCanSeeUsernameOnDashboard() {
+        dashboardPage.usernameIcon.click();
+    }
+
+    @When("user can click on customize button")
+    public void userCanClickOnCustomizeButton() {
+        BrowserUtils.sleep(2);
+        dashboardPage.customizeButton.click();
+        BrowserUtils.sleep(2);
+
+    }
+
+
+    @When("user can click on set weather widget")
+    public void userCanClickOnSetWeatherWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.weatherWidget.click();
+        BrowserUtils.sleep(2);
+    }
+
+    @And("user can click on status widget")
+    public void userCanClickOnStatusWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.statusWidget.click();
+        BrowserUtils.sleep(2);
+
+    }
+
+
+    @And("user can click on upcoming events widget")
+    public void userCanClickOnUpcomingEventsWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.upcomingEventsWidget.click();
+        BrowserUtils.sleep(2);
+    }
+
+    @And("user can click on upcoming cards widget")
+    public void userCanClickOnUpcomingCardsWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.upcomingCardsWidget.click();
+        BrowserUtils.sleep(2);
+
+    }
+
+
+    @And("user can click on recommended files widget")
+    public void userCanClickOnRecommendedFilesWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.recommendedFileWidget.click();
+        BrowserUtils.sleep(2);
+    }
+
+    @And("user can click on talk mentions widget")
+    public void userCanClickOnTalkMentionsWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.talkMentionWidget.click();
+        BrowserUtils.sleep(2);
+
+    }
+
+    @And("user can click on important mail widget")
+    public void userCanClickOnImportantMailWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.importantMailWidget.click();
+        BrowserUtils.sleep(2);
+
+    }
+
+    @And("user can click on customize button unread mail widget")
+    public void userCanClickOnCustomizeButtonUnreadMailWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.unreadMailWidget.click();
+        BrowserUtils.sleep(2);
+    }
+
+    @And("user can click on recent status widget")
+    public void userCanClickOnRecentStatusWidget() {
+        BrowserUtils.sleep(2);
+        dashboardPage.recentStatusesWidget.click();
+        BrowserUtils.sleep(2);
+    }
+    @When("user should be able to click on username icon")
+    public void userShouldBeAbleToClickOnUsernameIcon() {
+        dashboardPage.usernameIcon.click();
+
+ }
+
+
+
+    @And("user should be able to click on set status icon")
+    public void userShouldBeAbleToClickOnSetStatusIcon() {
+        dashboardPage.setStatusButton.click();
+    }
+
+
+    @And("user should be able to click on online status icon")
+    public void userShouldBeAbleToClickOnOnlineStatusIcon() {
+        dashboardPage.onlineStatusIcon.click();
+    }
+
+    @And("user should be able to click message status icon")
+    public void userShouldBeAbleToClickMessageStatusIcon() {
+        dashboardPage.statusMessageIcon.click();
+    }
+
+    @And("user should be able to click on away status icon")
+    public void userShouldBeAbleToClickOnAwayStatusIcon() {
+        dashboardPage.awayButtonIcon.click();
+    }
+
+    @And("user should be able to click on do not disturb status icon")
+    public void userShouldBeAbleToClickOnDoNotDisturbStatusIcon() {
+        dashboardPage.doNotDisturbBtn.click();
+
+    }
+
+    @And("user should be able to click on invisible status icon")
+    public void userShouldBeAbleToClickOnInvisibleStatusIcon() {
+        dashboardPage.invisibleStatusIcon.click();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/resources/features/dashboard.feature
@@ -0,0 +1,73 @@
+@dashboard
+  Feature: User should be able to modify Dashboard
+
+    Background: User is already in the log in page
+      Given the user is on the login page
+
+
+    Scenario: Verify modules list on dashboard
+      When  user can see all modules
+      |dashboard|
+      |files    |
+      |photos   |
+      |activity |
+      |spreed   |
+      |mail     |
+      |contacts |
+      |circles  |
+      |calendar |
+      |deck     |
+
+    Scenario: Verify username on dashboard
+       When user can see username on dashboard
+
+      Scenario: Verify customize button
+        When user can click on customize button
+
+        Scenario: Verify widget buttons
+          Given user can click on customize button
+          When user can click on set weather widget
+          And user can click on status widget
+          And user can click on upcoming events widget
+          And user can click on recommended files widget
+          And user can click on recent status widget
+          And user can click on talk mentions widget
+          And user can click on important mail widget
+          And user can click on customize button unread mail widget
+
+          Scenario: Verify set status button
+            When user should be able to click on username icon
+            And user should be able to click on set status icon
+
+            Scenario: Verify  online status option
+              Given user should be able to click on username icon
+              When user should be able to click on set status icon
+              And user should be able to click on online status icon
+              Then  user should be able to click message status icon
+
+              Scenario: Verify away status option
+                Given user should be able to click on username icon
+                When user should be able to click on set status icon
+                And user should be able to click on away status icon
+                Then user should be able to click message status icon
+
+
+                Scenario: Verify do not disturb option
+                  Given user should be able to click on username icon
+                  When user should be able to click on set status icon
+                  And user should be able to click on do not disturb status icon
+                  Then  user should be able to click message status icon
+
+                  Scenario: Verify invisible status option
+                    Given user should be able to click on username icon
+                    When user should be able to click on set status icon
+                    And user should be able to click on invisible status icon
+                    Then user should be able to click message status icon
+
+
+
+
+
+
+
+
